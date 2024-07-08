74ae5386c4c87872f8ad498dd33fa74ece6ef717
limboLecker <ruven.keveloh@web.de>
2024-07-08 11:09:59 +0200

    z coordinate
    adding z coordinate for movement depending on perspective

--- src/objects/cloud.py
+++ src/objects/cloud.py
@@ -7,14 +7,16 @@ class Cloud:
     def __init__(self):
         self.x = SCREEN_WIDTH + random.randint(0, SCREEN_WIDTH)
         self.y = random.randint(50, 100)
+        self.z = random.randint(3, 9)
         self.image = CLOUD
         self.width = self.image.get_width()
 
     def update(self, game_speed):
-        self.x -= 0.3 * game_speed
+        self.x -= game_speed/self.z
         if self.x < -self.width:
             self.x = SCREEN_WIDTH + random.randint(2500, 3000)
             self.y = random.randint(50, 100)
+            self.z = random.randint(3, 9)
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.x, self.y))
