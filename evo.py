7d1a99b8b6107979b7aa3d75b3bfe5522b4386c6
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2024-07-08 09:05:02 +0200

    Initial commit

---
+++ evo.py
@@ -0,0 +1,25 @@
+import numpy as np
+import random
+
+
+def activation(x: float) -> float:
+    return 1 / (1 + np.exp(-x))
+
+def prediction(
+        x: float,
+        y: float,
+        weight_x: float,
+        weight_y: float,
+        b: float
+    ) -> float:
+    return activation(x * weight_x + y * weight_y + b)
+
+
+def crossbreed(parent1, parent2):
+    return [parent1[0]] + parent2[1:]
+
+def mutate(chromosome):
+    chromosome[random.randint(0, 2)] += np.random.randn()
+
+def breed():
+    
