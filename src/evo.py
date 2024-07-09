ec31d591fadcb4ba502ee0e24b97d697f4c29b07
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2024-07-09 17:09:57 +0200

    Update prediction algorithm

--- src/evo.py
+++ src/evo.py
@@ -2,24 +2,21 @@ import numpy as np
 import random
 
 
-def activation(x: float) -> float:
-    return 1 / (1 + np.exp(-x))
+def activation(input_values: np.ndarray) -> np.ndarray:
+    return 1 / (1 + np.exp(-input_values))
 
 def prediction(
-        x: float,
-        y: float,
-        weight_x: float,
-        weight_y: float,
-        b: float
-    ) -> float:
-    return activation(x * weight_x + y * weight_y + b)
+        input_values: np.ndarray,
+        weights: np.ndarray,
+        biases: np.ndarray
+    ) -> np.ndarray:
+    return activation(weights @ input_values + biases)
 
 
 def crossbreed(parent1, parent2):
     return [parent1[0]] + parent2[1:]
 
-def mutate(chromosome):
-    chromosome[random.randint(0, 2)] += np.random.randn()
+def mutate(genome):
+    genome[random.randint(0, 2)] += np.random.randn()
 
 # def breed():
-    
