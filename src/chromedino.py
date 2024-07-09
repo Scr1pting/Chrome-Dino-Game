c129fb1261bc010c7cd8c5dc4da68d08f6d92b96
limboLecker <ruven.keveloh@web.de>
2024-07-09 02:38:46 +0200

    comments and some commented functions

--- src/chromedino.py
+++ src/chromedino.py
@@ -1,19 +1,21 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# lib
 import datetime
 import os
 import random
 import threading
-
 import pygame # type: ignore
 
 from settings import *
 
+#object import Cactus, Bird, Cloud
 from objects.obstacles import SmallCactus, LargeCactus, Bird
 from objects.dinosaur import Dinosaur
 from objects.cloud import Cloud
 
-
+#init
 pygame.init()
 
 
@@ -38,17 +40,21 @@ def main():
     obstacles = []
     death_count = 0
 
+    frame =0
+    framesPerGenerate = 30
+
     def score():
         global points, distance, game_speed, highscore
         distance += game_speed
 
+        
         if distance % 50 == 0:
             points += 1
 
         if distance % 1000 == 0:
             game_speed += 1
         
-        with open("../highscore.txt", "w") as f:
+        with open("../highscore.txt", "w+") as f:
             if points >= highscore:
                 highscore = points
                 f.write(str(highscore))
@@ -77,17 +83,22 @@ def main():
             SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
             x_pos_bg = 0
         x_pos_bg -= game_speed
-
+    """
+    def generateObjects()
+        switch{0:SmallCactus(SMALL_CACTUS), 1:LargeCactus(LARGE_CACTUS), 2:Bird(BIRD)}
+        obstacles.append(switch[random.randint(0, 2)])
+    """
     while run:
         is_dead = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        
         SCREEN.fill(BACKGROUND_COLOR)
         background()
-
+        
+        #get input
         userInput = pygame.key.get_pressed()
         player.update(userInput)
 
@@ -99,31 +110,48 @@ def main():
                 obstacles.append(LargeCactus(LARGE_CACTUS))
             elif switch == 2:
                 obstacles.append(Bird(BIRD))
-
+        """
+        if len(obstacles) == 0:
+            switch{0:SmallCactus(SMALL_CACTUS), 1:LargeCactus(LARGE_CACTUS), 2:Bird(BIRD)}
+            obstacles.append(switch[random.randint(0, 2)])
+        """
+        #obstacles
         for obstacle in obstacles:
+            #draw call
             obstacle.draw(SCREEN)
             obstacle.update(game_speed, obstacles)
-
+            
+            #collision detection
             if pygame.sprite.collide_mask(player, obstacle):
                 player.dead()
                 is_dead = True
                 death_count += 1
-
+        
+        #draw call for player
         player.draw(SCREEN)
-
+        #draw call for clouds
         for cloud in clouds:
             cloud.update(game_speed)
             cloud.draw(SCREEN)
-
+        
+        #update score
         score()
-
+        
+        #render frame
         pygame.display.update()
-
+        
+        #generating 
+        """
+        if(frame%framesPerGenerate==0)
+            generateObjects()
+        """
+        #switch to menu
         if is_dead:
             pygame.time.delay(2000)
             menu(death_count)
 
         clock.tick(FRAME_RATE)
+        frame
 
 
 # MARK: Menu
@@ -133,14 +161,14 @@ def menu(death_count):
 
     highscore = 0
     run = True
-
-    with open("../highscore.txt", "r") as f:
+    #highscore file for memory
+    with open("../highscore.txt", "w+") as f:
         raw_highscore = f.read().strip()
         try:
             highscore = int(raw_highscore)
         except:
             highscore = 0
-
+    #update menu loop
     while run:
         SCREEN.fill(BACKGROUND_COLOR)
         font = pygame.font.Font(FONT_FAMILY, 20)
@@ -178,6 +206,6 @@ def menu(death_count):
             if event.type == pygame.KEYDOWN:
                 main()
 
-
+#start thread
 t1 = threading.Thread(target=menu(death_count=0), daemon=True)
 t1.start()
