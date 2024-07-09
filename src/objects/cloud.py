c26802c51a23ff1e0bf2ede928e8924ec280afcc
limboLecker <ruven.keveloh@web.de>
2024-07-09 11:16:11 +0200

    Update cloud.py
    cloud distances

--- src/objects/cloud.py
+++ src/objects/cloud.py
@@ -11,12 +11,11 @@ class Cloud:
         self.image = CLOUD
         self.width = self.image.get_width()
 
-    def update(self, game_speed):
+    def update(self, game_speed, clouds, i):
         self.x -= game_speed/self.z
         if self.x < -self.width:
-            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.x = clouds[(i+2)%3].x +  random.randint(1500, 2000)
             self.y = random.randint(50, 100)
-            self.z = random.randint(3, 9)
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.x, self.y))
